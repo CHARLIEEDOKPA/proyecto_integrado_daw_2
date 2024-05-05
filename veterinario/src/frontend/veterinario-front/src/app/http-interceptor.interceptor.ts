fa88160a0b98db558e2370241fd323deff23aa05
Charlie Onosetale <jnr.31gcharlieedokpa@gmail.com>
2024-05-05 21:49:52 +0200

    Modifiqué el http-interceptor por un problema

--- veterinario/src/frontend/veterinario-front/src/app/http-interceptor.interceptor.ts
+++ veterinario/src/frontend/veterinario-front/src/app/http-interceptor.interceptor.ts
@@ -5,11 +5,15 @@ import { inject } from '@angular/core';
 export const httpInterceptorInterceptor: HttpInterceptorFn = (req, next) => {
   let jwtService = inject(JwtService)
   let token = jwtService.getTokenFromLocalStorage()
-  
-  let cloneReq = req.clone({
-    setHeaders: {
-      Authorization: `Bearer ${token}`
-    }
-  })
-  return next(cloneReq);
+  let url = req.url
+  if(!url.endsWith("login")) {
+    let cloneReq = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+    return next(cloneReq);
+  }
+  return next(req)
+ 
 };
